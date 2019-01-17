c84c6e777d5951ed321e2171bb22d0597f837e58
diescake <daisuke.kondo@access-company.com>
2019-01-18 01:06:02 +0900

    Managed assets

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,7 @@
-document.body.style.backgroundColor = 'green'
+import image from '@/assets/images/diescake.png'
+import '@/assets/css/common.css'
+
+const imageElem = document.createElement('img')
+imageElem.setAttribute('src', image);
+
+document.body.appendChild(imageElem)
--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,20 @@ module.exports = (env, argv) => {
   return {
     entry: './src/main.tsx',
     output: {
-      filename: 'main.js',
+      filename: 'bundle.js',
       path: path.resolve(__dirname, 'public'),
-    }
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+    module: {
+      rules: [
+        { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+        { test: /\.(png|svg|jpg|gif)$/, use: 'file-loader' },
+        { test: /\.(woff|woff2|eot|ttf|otf)$/, use: 'file-loader' },
+      ],
+    },
   }
 }
