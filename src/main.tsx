854573a71658b26b2d297af85660fabfc644e63a
diescake <daisuke.kondo@access-company.com>
2019-01-22 01:30:14 +0900

    Hello3

--- src/main.tsx
+++ src/main.tsx
@@ -10,14 +10,9 @@ if (process.env.NODE_ENV !== 'production') {
 
 const root = document.getElementById('root')
 
-const DUMMY_NUM_1 = 123
-const DUMMY_NUM_2 = 35
-const DUMMY_MAG = 2
-
 interface HelloProps {
-  compiler: string
-  framework: string
   defaultValue: number
+  mag: number
 }
 
 interface HelloState {
@@ -27,38 +22,40 @@ interface HelloState {
 export class Hello extends React.Component<HelloProps, HelloState> {
   constructor(props: HelloProps) {
     super(props)
-
     this.state = {
       value: props.defaultValue,
     }
   }
 
+  static defaultProps: Pick<HelloProps, 'mag' | 'defaultValue'> = {
+    mag: 10,
+    defaultValue: 1,
+  }
+
   handleClick = () => {
     this.setState({
-      value: this.state.value * DUMMY_MAG,
+      value: this.state.value * this.props.mag,
     })
-    console.log(this.props.defaultValue)
   }
 
   multiply = (mag: number) => this.state.value * mag
 
   render() {
     return (
-      <>
-        <h1>
-          Hello from {this.props.compiler} and {this.props.framework}!
-        </h1>
-        <div>{this.multiply(DUMMY_MAG)}</div>
-        <button onClick={this.handleClick}>Click Me</button>
-      </>
+      <div>
+        <button onClick={this.handleClick}>Multiply</button>
+        <span>{this.state.value}</span>
+      </div>
     )
   }
 }
 
 ReactDOM.render(
   <>
-    <Hello compiler="TypeScript" framework="React" defaultValue={DUMMY_NUM_1} />
-    <Hello compiler="TypeScript" framework="React" defaultValue={DUMMY_NUM_2} />
+    <h1>Hello from TypeScript and framework!!!</h1>
+    <Hello defaultValue={123} mag={2} />
+    <Hello />
+    <br />
     <img src={diescakePng} />
   </>,
   root
