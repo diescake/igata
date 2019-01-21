dd8d593b20903a7ceca59645ff9585940b3c6675
diescake <daisuke.kondo@access-company.com>
2019-01-21 17:04:59 +0900

    Add React and implement the hello

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,37 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
 import '@/assets/css/common.scss'
 import diescakePng from '@/assets/images/diescake.png'
 
 if (process.env.NODE_ENV !== 'production') {
-  console.warn('Build with development mode')
+  console.info('Build with development mode')
 }
 
-const multiply = (num: number, multi: number): number => {
-  return num * multi
-}
-
-const imageElem = document.createElement('img')
-imageElem.setAttribute('src', diescakePng)
-
-document.body.appendChild(imageElem)
+const root = document.getElementById('root')
 
 const DUMMY_NUM = 123
 const DUMMY_MULTI = 2
 
-console.log(multiply(DUMMY_NUM, DUMMY_MULTI))
+interface HelloProps {
+  compiler: string
+  framework: string
+}
+
+export class Hello extends React.Component<HelloProps, {}> {
+  multiply = (num: number, mag: number) => num * mag
+
+  render() {
+    return (
+      <div>
+        <h1>
+          Hello from {this.props.compiler} and {this.props.framework}!
+        </h1>
+        <img src={diescakePng} />
+        <span>{this.multiply(DUMMY_NUM, DUMMY_MULTI)}</span>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Hello compiler="TypeScript" framework="React" />, root)
