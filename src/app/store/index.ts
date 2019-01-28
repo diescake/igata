16fca9c551d37f872eeb84d6ef31e6bd7edc1c23
diescake <daisuke.kondo@access-company.com>
2019-01-28 18:52:25 +0900

    🛠 Add redux-logger and set my configuration

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -2,9 +2,20 @@ import createRootReducer from '@/app/reducers/todo'
 import { routerMiddleware } from 'connected-react-router'
 import { History } from 'history'
 import { applyMiddleware, compose, createStore } from 'redux'
+import { createLogger } from 'redux-logger'
+
+// redux-logger options
+// https://github.com/LogRocket/redux-logger#options
+const loggerOption = {
+  collapsed: true,
+  diff: true,
+}
 
 export const configureStore = (history: History) => {
-  const store = createStore(createRootReducer(history), compose(applyMiddleware(routerMiddleware(history))))
+  const store = createStore(
+    createRootReducer(history),
+    compose(applyMiddleware(routerMiddleware(history), createLogger(loggerOption)))
+  )
 
   return store
 }
