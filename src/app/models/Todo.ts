35b1e49add5dc1c9467d36e04dfbb02eae9f3dae
diescake <daisuke.kondo@access-company.com>
2019-03-04 05:43:28 +0900

    Remove no longer used models

--- src/app/models/Todo.ts
+++ src/app/models/Todo.ts
@@ -1,13 +1,3 @@
-// Types
-// export type Text = string
-
-// Actions
-export interface TodoAction {
-  type: string
-  text: string
-}
-
-// States
 export interface Todo {
   done: boolean
   text: string
