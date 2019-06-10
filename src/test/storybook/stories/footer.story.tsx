924f67a22c5003b74470d308191693190598de45
Kensuke Takahara <zawcjk@gmail.com>
2019-06-10 22:53:37 +0900

    [#228] add storybook package & sample test code.

---
+++ src/test/storybook/stories/footer.story.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import { storiesOf } from '@storybook/react'
+import { Footer } from '@/app/components/Footer'
+
+const stories = storiesOf('Components', module)
+
+stories.add('Footer', () => <Footer />)
