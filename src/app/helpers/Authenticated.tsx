03a38cb09e0d7fee307a049d8149f1fb1c5a29de
diescake <daisuke.kondo@access-company.com>
2019-06-11 07:02:56 +0900

    Transform Authenticated to FC

--- src/app/helpers/Authenticated.tsx
+++ src/app/helpers/Authenticated.tsx
@@ -2,16 +2,13 @@ import * as React from 'react'
 import { Redirect } from 'react-router'
 
 interface Props {
-  children: React.ReactNode
+  children?: React.ReactNode
 }
-interface State {}
 
-export class Authenticated extends React.Component<Props, State> {
-  render = () => {
-    if (!localStorage.getItem('token')) {
-      return <Redirect to="/login" />
-    }
-
-    return <>{this.props.children}</>
+export const Authenticated = (props: Props) => {
+  if (!localStorage.getItem('token')) {
+    return <Redirect to="/login" />
   }
+
+  return <>{props.children}</>
 }
