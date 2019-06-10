af907088abd0c5543a3c134c43ec3d37b1fa5640
diescake <daisuke.kondo@access-company.com>
2019-06-11 06:57:20 +0900

    Transform ListWrapper to FC

--- src/app/components/ListWrapper/index.tsx
+++ src/app/components/ListWrapper/index.tsx
@@ -2,16 +2,13 @@ import * as React from 'react'
 import style from './style.scss'
 
 interface Props {
-  children: React.ReactNode
+  children?: React.ReactNode
 }
-interface State {}
 
-export class ListWrapper extends React.Component<Props, State> {
-  render = () => {
-    if (!this.props.children || !React.Children.count(this.props.children)) {
-      return null
-    }
-
-    return <ul className={style.listWrapperUl}>{this.props.children}</ul>
+export const ListWrapper = (props: Props) => {
+  if (!props.children || React.Children.count(props.children) === 0) {
+    return null
   }
+
+  return <ul className={style.listWrapperUl}>{props.children}</ul>
 }
