de8979328311aaa654c819838c7d8c75ae3bdd1f
diescake <daisuke.kondo@access-company.com>
2019-06-13 04:24:58 +0900

    Prepare for CORS

--- src/app/sagas/todoApp.ts
+++ src/app/sagas/todoApp.ts
@@ -3,7 +3,8 @@ import { TodoState } from '@/app/models/Todo'
 import axios, { AxiosResponse } from 'axios'
 import { call, put, takeLatest } from 'redux-saga/effects'
 
-const TODOS_JSON_URL = 'https://raw.githubusercontent.com/diescake/igata/master/data/todos.json'
+// NOTE: "myjson.com" supports CORS and allows basic headers and methods.
+const TODOS_JSON_URL = 'https://api.myjson.com/bins/81wtd'
 
 function* fetchTodos() {
   const res: AxiosResponse<TodoState> = yield call(axios.get, TODOS_JSON_URL)
