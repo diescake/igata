9a2afd3942d779bbd14ca651dea8b20505243451
diescake <daisuke.kondo@access-company.com>
2019-06-14 07:13:55 +0900

    ♻ Request login

--- src/app/sagas/login.ts
+++ src/app/sagas/login.ts
@@ -1,18 +1,24 @@
 import { loginFailure, loginSuccess, Type } from '@/app/actions/login'
 import { LoginState } from '@/app/models/Login'
-import axios, { AxiosResponse } from 'axios'
+import { get, HttpResponse } from '@/app/helpers/http'
 import { call, put, takeLatest } from 'redux-saga/effects'
 
 const LOGIN_JSON_URL = 'https://raw.githubusercontent.com/diescake/igata/master/data/login.json'
 
 function* login() {
   // NOTE: Actually the login request should be POST
-  const res: AxiosResponse<LoginState> = yield call(axios.get, LOGIN_JSON_URL)
+  const { res, error }: HttpResponse<LoginState> = yield call(get, LOGIN_JSON_URL, false)
+  if (error) {
+    yield put(loginFailure(error.message))
+    return
+  }
 
-  if (res.data) {
-    yield put(loginSuccess(res.data))
-  } else {
-    yield put(loginFailure(res.statusText))
+  switch (res.status) {
+    case 200:
+      yield put(loginSuccess(res.data))
+      break
+    default:
+      yield put(loginFailure(res.statusText))
   }
 }
 
