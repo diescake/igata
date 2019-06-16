faafb0a631368b84175afd3f7f15f5a6c132af0c
diescake <daisuke.kondo@access-company.com>
2019-06-16 18:48:53 +0900

    Support importing images and styles on Storybook

--- src/test/storybook/config/webpack.config.js
+++ src/test/storybook/config/webpack.config.js
@@ -2,15 +2,36 @@
 const path = require('path')
 
 module.exports = ({ config }) => {
+  // eslint-disable-next-line no-param-reassign
+  config.resolve.alias = { '@': path.resolve(__dirname, '../../..') }
+  config.resolve.extensions.push('.ts', '.tsx')
+
   config.module.rules.push(
     {
       test: /\.(ts|tsx)$/,
       use: ['ts-loader', 'react-docgen-typescript-loader'],
     },
-    { test: /\.(yaml|yml)$/, use: ['json-loader', 'yaml-loader'] }
+    { test: /\.(yaml|yml)$/, use: ['json-loader', 'yaml-loader'] },
+    { test: /\.(png|svg|jpg|gif)$/, use: 'file-loader' },
+    { test: /\.(woff|woff2|eot|ttf|otf)$/, use: 'file-loader' },
+    {
+      test: /\.(css|scss|sass)$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: {
+              localIdentName: '[local]__[hash:base64:5]',
+            },
+            importLoaders: 1,
+            sourceMap: true,
+          },
+        },
+        'sass-loader',
+      ],
+    }
   )
-  // eslint-disable-next-line no-param-reassign
-  config.resolve.alias = { '@': path.resolve(__dirname, '../../..') }
-  config.resolve.extensions.push('.ts', '.tsx')
+
   return config
 }
