3710d95307064c1db7d5e60ec51314a356f6decb
diescake <daisuke.kondo@access-company.com>
2019-06-20 20:35:21 +0900

    Add storybook addons

--- src/test/storybook/config/config.ts
+++ src/test/storybook/config/config.ts
@@ -1,12 +1,20 @@
-import { configure } from '@storybook/react'
-
 import '@/assets/css/reboot.css'
 import '@/assets/css/common.scss'
+
+import '@storybook/addon-a11y/register'
+import '@storybook/addon-actions/register'
+import '@storybook/addon-knobs/register'
+import '@storybook/addon-links/register'
+import { configure, addDecorator } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
+import { withKnobs } from '@storybook/addon-knobs'
 
 // automatically import all files ending in *.stories.tsx
 const req = require.context('../stories', true, /\.story\.(ts|tsx)$/)
 
 function loadStories() {
+  addDecorator(withInfo)
+  addDecorator(withKnobs)
   req.keys().forEach(req)
 }
 
