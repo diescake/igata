f2b0fb8142c3cda23eaba7c649094f54cf319c25
diescake <daisuke.kondo@access-company.com>
2019-06-20 22:24:02 +0900

    Improve storybook

--- src/test/storybook/stories/main.story.tsx
+++ src/test/storybook/stories/main.story.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { text } from '@storybook/addon-knobs'
+import { text, boolean } from '@storybook/addon-knobs'
 import { Header } from '@/app/components/Header'
 import { Footer } from '@/app/components/Footer'
 import { ListWrapper } from '@/app/components/ListWrapper'
+import { Modal } from '@/app/components/Modal'
 // import Login from '@/app/containers/Login'
 
 // FIXME: Preparing connect I/Fs to import container components
@@ -20,12 +21,26 @@ storiesOf('Components|Footer', module)
   .add('with @pankona', () => <Footer copyright={text('copyright', 'pankona')} />)
   .add('with no props', () => <Footer copyright={text('copyright', '')} />)
 
+const createModal = (hidden: boolean) => (
+  <>
+    <p>--- outside of modal ---</p>
+    <Modal hidden={boolean('hidden', hidden)} name={text('name', 'Sample dialog')} close={() => console.log('close')}>
+      <p>This is a sample modal description.</p>
+    </Modal>
+    <p>--- outside of modal ---</p>
+  </>
+)
+
+storiesOf('Components|Modal', module)
+  .add('hidden', () => createModal(true))
+  .add('not hidden', () => createModal(false))
+
 storiesOf('Components|ListWrapper', module)
   .add('with lists', () => (
     <ListWrapper>
-      <li>item 1</li>
-      <li>item 2</li>
-      <li>item 3</li>
+      <li>{text('item_01', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ')}</li>
+      <li>{text('item_02', 'Lorem Ipsum has been the industry standard dummy text ever since the 1500s')}</li>
+      <li>{text('item_03', 'when an unknown printer took a galley of type and scrambled it to make a type specimen book.')}</li>
     </ListWrapper>
   ))
   .add('with no props', () => <ListWrapper />)
