1c511cb4b2a517beb9626cb8f35a1e158492333a
diescake <daisuke.kondo@access-company.com>
2019-06-30 03:39:52 +0900

    Add input validation functions

--- src/app/helpers/utils.ts
+++ src/app/helpers/utils.ts
@@ -1,1 +1,8 @@
 export const nlToBr = (text: string) => text.replace(/(?:\r\n|\r|\n)/g, '<br/>')
+
+export const isValidEmail = (text: string) =>
+  new RegExp(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/).test(text)
+
+export const isValidPassword = (text: string) => new RegExp(/^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[\x20-\x7e]*$/).test(text)
+export const isValidKatakana = (text: string) => new RegExp(/^[\u30a1-\u30FC]+$/).test(text)
+export const isValidPhoneNumber = (text: string) => new RegExp(/^([0-9]|-|\+)+$/).test(text)
