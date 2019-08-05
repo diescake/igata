8894fe66314e06cb3318802436d6a3ca1545d6bf
diescake <daisuke.kondo@access-company.com>
2019-08-05 22:04:32 +0900

    Improve readability for the action types

--- src/app/common/typeHelper.ts
+++ src/app/common/typeHelper.ts
@@ -1,9 +1,26 @@
-type ExtractFunctionReturnTypes<T> = { [K in keyof T]: T[K] extends (...args: any) => any ? ReturnType<T[K]> : never }
-
+// General
 type Unbox<T> = T extends { [K in keyof T]: infer U } ? U : never
+
+// Actions
+interface Action {
+  type: any
+  payload?: any
+}
+
+type ActionCreator = (...args: any) => Action
+type IsActionCreator<T> = T extends ActionCreator ? T : never
+
+type ExtractFunctionReturnType<T> = ReturnType<IsActionCreator<T>>
+
+type ExtractFunctionReturnTypes<T> = {
+  [K in keyof T]: ExtractFunctionReturnType<T[K]>
+}
 
 export type CreateActionTypes<T> = Unbox<ExtractFunctionReturnTypes<T>>
 
+type ExtractFunctionParameterType<T> = Parameters<IsActionCreator<T>>
+type CreateDispatcherType<T> = (...args: ExtractFunctionParameterType<T>) => void
+
 export type CreateDispatcherTypes<T> = {
-  [K in keyof T]: T[K] extends (...args: any) => any ? (...args: Parameters<T[K]>) => void : never
+  [K in keyof T]: CreateDispatcherType<T[K]>
 }
