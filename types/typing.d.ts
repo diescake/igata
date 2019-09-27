fab3b840b6bdb6bda63568abf916d8cae1a56709
Daisuke Kondo <daisuke.kondo@access-company.com>
2019-09-27 15:21:14 +0900

    Update typing.d.ts

--- types/typing.d.ts
+++ types/typing.d.ts
@@ -23,6 +23,11 @@ declare module '*.png' {
   export default content
 }
 
+declare module '*.svg' {
+  const content: string
+  export default content
+}
+
 declare module '*.txt' {
   const content: string
   export default content
