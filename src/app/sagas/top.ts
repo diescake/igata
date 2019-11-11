1de73ff6021566283bf797d59b5387c44c895b5d
kazumasa.shiomi <kazumasa.shiomi@access-company.com>
2019-11-11 18:57:28 +0900

    パスの修正。

--- src/app/sagas/top.ts
+++ src/app/sagas/top.ts
@@ -5,7 +5,7 @@ import { QuestionsResponse } from '@/app/models/HttpResponse'
 import { Question } from '@/app/models/Question'
 import { get, HttpResponse } from '@/app/common/http'
 
-const QUESTIONS_JSON_URL = 'https://stackoverflow-clone.solomondev.access-company.com/v1/question'
+const QUESTIONS_JSON_URL = 'https://api.myjson.com/bins/17rf2l'
 
 const isQuestionsResponse = (props: any): props is QuestionsResponse => {
   try {
